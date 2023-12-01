1bcd8693987943e9ea41d7ba269426a1d872394e
Tomas del Real <tdelrealb@gmail.com>
2023-11-30 23:22:31 -0300

    Update Routes

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -8,7 +8,7 @@ router.use(validateToken);
 
 router.post('/create', taskController.createTask);
 router.get('/all', taskController.getTasks);
-router.get('/filter/area/:area', taskController.filterTasksByArea);
+router.get('/filter/project/:projectId', taskController.filterTasksByProject);
 router.get('/filter/label/:label', taskController.filterTasksByLabel);
 router.put('/edit/:id', taskController.editTask);
 router.delete('/delete/:id', taskController.deleteTask);
