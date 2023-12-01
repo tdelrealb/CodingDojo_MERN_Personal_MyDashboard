9a92989300dab0c0e34ff09c22cb65b144885d3f
Tomas del Real <tdelrealb@gmail.com>
2023-12-01 00:24:21 -0300

    Add Login and Register Basic Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { SignUp } from './pages/SignUp/SignUp';
 import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
 	return (
 		<div className='app'>
-			<Routes></Routes>
+			<Routes>
+				<Route exact path='/' Component={Home} />
+				<Route exact path='/login' Component={Login} />
+				<Route exact path='/signup' Component={SignUp} />
+			</Routes>
 		</div>
 	);
 };
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import styles from './Home.module.css';
+export const Home = () => {
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+};
