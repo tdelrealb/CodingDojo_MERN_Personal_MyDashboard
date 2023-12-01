5195a513816d5efe54d65d54185f892ac52ee0be
Tomas del Real <tdelrealb@gmail.com>
2023-11-30 23:27:48 -0300

    Client Initial Commit

--- server/src/middlewares/cors.middleware.js
+++ server/src/middlewares/cors.middleware.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsConfig = cors({
-	origin: '*',
+	origin: 'http://localhost:5173/',
 	methods: 'GET,POST,PUT,DELETE',
 	allowedHeaders: '*',
 	exposedHeaders: '*',
