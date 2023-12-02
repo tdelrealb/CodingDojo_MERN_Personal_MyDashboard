122b11bf438c08449192a086a2f45e06b25fcad4
Tomas del Real <tdelrealb@gmail.com>
2023-12-02 17:56:44 -0300

    Update Server Config

--- server/src/config/server.config.js
+++ server/src/config/server.config.js
@@ -5,6 +5,10 @@ import * as userRoutes from '../routes/user.routes.js';
 import * as projectRoutes from '../routes/project.routes.js';
 import * as taskRoutes from '../routes/task.routes.js';
 import * as noteRoutes from '../routes/note.routes.js';
+import * as resourceRoutes from '../routes/resource.routes.js';
+import * as habitRoutes from '../routes/habit.routes.js';
+import * as incomeRoutes from '../routes/income.routes.js';
+import * as expenseRoutes from '../routes/expense.routes.js';
 
 dotenv.config();
 
@@ -18,5 +22,9 @@ app.use('/users', userRoutes.router);
 app.use('/projects', projectRoutes.router);
 app.use('/tasks', taskRoutes.router);
 app.use('/notes', noteRoutes.router);
+app.use('/resources', resourceRoutes.router);
+app.use('/habits', habitRoutes.router);
+app.use('/incomes', incomeRoutes.router);
+app.use('/expenses', expenseRoutes.router);
 
 export default app;
