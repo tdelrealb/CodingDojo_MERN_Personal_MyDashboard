2eba4ace1fa0a407fe40d3e50795e1b9deda6b71
Tomas del Real <tdelrealb@gmail.com>
2023-12-02 17:50:43 -0300

    Update Note Controller

--- server/src/controllers/note.controller.js
+++ server/src/controllers/note.controller.js
@@ -1,4 +1,5 @@
 import Note from '../models/note.model.js';
+import Project from '../models/project.model.js';
 
 const createNote = async (req, res) => {
 	const noteData = req.body;
@@ -7,24 +8,13 @@ const createNote = async (req, res) => {
 	try {
 		const newNote = await Note.create({ ...noteData, userId });
 		await newNote.save();
-		res.status(201).json({ message: newNote });
+		res.status(201).json({ newNote });
 	} catch (error) {
 		res.status(500).json({ error: 'Error creating the note.' });
 	}
 };
 
-const getNotes = async (req, res) => {
-	const userId = req.user._id;
-
-	try {
-		const notes = await Note.find({ userId });
-		res.status(200).json({ notes });
-	} catch (error) {
-		res.status(500).json({ error: 'Error loading notes.' });
-	}
-};
-
-const filterNotesByArea = async (req, res) => {
+const getNotesByArea = async (req, res) => {
 	const { area } = req.params;
 	const userId = req.user._id;
 
@@ -36,7 +26,7 @@ const filterNotesByArea = async (req, res) => {
 	}
 };
 
-const filterNotesByLabel = async (req, res) => {
+const getNotesByLabel = async (req, res) => {
 	const { label } = req.params;
 	const userId = req.user._id;
 
@@ -58,6 +48,7 @@ const searchNotes = async (req, res) => {
 			$or: [
 				{ title: { $regex: query, $options: 'i' } },
 				{ body: { $regex: query, $options: 'i' } },
+				{ label: { $regex: query, $options: 'i' } },
 			],
 		});
 		res.status(200).json({ notes });
@@ -66,22 +57,35 @@ const searchNotes = async (req, res) => {
 	}
 };
 
-const editNote = async (req, res) => {
+const updateNote = async (req, res) => {
 	const { id } = req.params;
-	const { area, title, label, body } = req.body;
+	const { projectId, area, title, label, body } = req.body;
 	const userId = req.user._id;
 
 	try {
-		const updatedNote = await Note.findOneAndUpdate(
-			{ _id: id, userId },
-			{ area, title, label, body },
-			{ new: true, runValidators: true },
-		);
+		const note = await Note.findOne({ _id: id, userId });
 
-		if (!updatedNote) {
-			res.status(404).json({ error: 'Note not found.' });
+		if (!note) {
+			return res.status(404).json({ error: 'Note not found.' });
 		}
-		res.status(200).json({ note: updatedNote });
+
+		if (projectId) {
+			const project = await Project.findOne({ _id: projectId, userId });
+
+			if (!project) {
+				return res.status(404).json({ error: 'Project not found.' });
+			}
+
+			note.projectId = projectId;
+		}
+
+		note.area = area;
+		note.title = title;
+		note.label = label;
+		note.body = body;
+
+		await note.save();
+		res.status(200).json({ note });
 	} catch (error) {
 		res.status(500).json({ error: 'Error updating the note.' });
 	}
@@ -95,9 +99,7 @@ const deleteNote = async (req, res) => {
 		const note = await Note.findByIdAndDelete({ _id: id, userId });
 
 		if (!note) {
-			return res
-				.status(404)
-				.json({ error: 'We were unable to find the note.' });
+			return res.status(404).json({ error: 'Note not found.' });
 		}
 		res.status(200).json({ message: 'Note succesfully deleted.' });
 	} catch (error) {
@@ -107,10 +109,9 @@ const deleteNote = async (req, res) => {
 
 export {
 	createNote,
-	getNotes,
-	filterNotesByArea,
-	filterNotesByLabel,
+	getNotesByArea,
+	getNotesByLabel,
 	searchNotes,
-	editNote,
+	updateNote,
 	deleteNote,
 };
