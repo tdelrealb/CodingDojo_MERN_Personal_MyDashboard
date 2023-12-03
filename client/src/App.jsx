b7dac99ec3704ff6cfdbd4e82ad78b6a83487e11
Tomas del Real <tdelrealb@gmail.com>
2023-12-03 15:55:59 -0300

    Add Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,27 @@ import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { SignUp } from './pages/SignUp/SignUp';
 import './styles/App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
 	return (
 		<div className='app'>
-			<Routes>
-				<Route exact path='/' Component={Home} />
-				<Route exact path='/login' Component={Login} />
-				<Route exact path='/signup' Component={SignUp} />
-			</Routes>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path='/' Component={Home} />
+					<Route exact path='/login' Component={Login} />
+					<Route exact path='/signup' Component={SignUp} />
+					<Route exact path='/dashboard'/>
+					<Route exact path='/work'/>
+					<Route exact path='/studies'/>
+					<Route exact path='/trip'/>
+					<Route exact path='/personal'/>
+					<Route exact path='/finance'/>
+					<Route exact path='/social'/>
+					<Route exact path='/hobbies'/>
+
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 };
