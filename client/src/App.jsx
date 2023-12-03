80ac469fec98c0f36693fe86be9bebf3e0433c6b
Tomas del Real <tdelrealb@gmail.com>
2023-12-03 16:39:40 -0300

    Config Private Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { SignUp } from './pages/SignUp/SignUp';
-import {Dashboard} from './pages/Dashboard/Dashboard';
-import {Work} from './pages/Work/Work';
-import {Studies} from './pages/Studies/Studies';
-import {Trip} from './pages/Trip/Trip';
-import {Personal} from './pages/Personal/Personal';
-import {Finance} from './pages/Finance/Finance';
-import {Social} from './pages/Social/Social';
-import {Hobbies} from './pages/Hobbies/Hobbies';
+import { Dashboard } from './pages/Dashboard/Dashboard';
+import { Work } from './pages/Work/Work';
+import { Studies } from './pages/Studies/Studies';
+import { Trip } from './pages/Trip/Trip';
+import { Personal } from './pages/Personal/Personal';
+import { Finance } from './pages/Finance/Finance';
+import { Social } from './pages/Social/Social';
+import { Hobbies } from './pages/Hobbies/Hobbies';
+import { AuthProvider } from './utils/AuthProvider';
 
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -18,18 +19,74 @@ export const App = () => {
 		<div className='app'>
 			<BrowserRouter>
 				<Routes>
-					<Route exact path='/' Component={Home} />
-					<Route exact path='/login' Component={Login} />
-					<Route exact path='/signup' Component={SignUp} />
-					<Route exact path='/dashboard' Component={Dashboard}/>
-					<Route exact path='/work' Component={Work}/>
-					<Route exact path='/studies' Component={Studies}/>
-					<Route exact path='/trip' Component={Trip}/>
-					<Route exact path='/personal' Component={Personal}/>
-					<Route exact path='/finance' Component={Finance}/>
-					<Route exact path='/social' Component={Social}/>
-					<Route exact path='/hobbies' Component={Hobbies}/>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />}></Route>
+					<Route path='/signup' element={<SignUp />}></Route>
 
+					{/* Private Routes */}
+					<Route
+						path='/dashboard'
+						element={
+							<AuthProvider>
+								<Dashboard />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/work'
+						element={
+							<AuthProvider>
+								<Work />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/studies'
+						element={
+							<AuthProvider>
+								<Studies />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/trip'
+						element={
+							<AuthProvider>
+								<Trip />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/personal'
+						element={
+							<AuthProvider>
+								<Personal />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/finance'
+						element={
+							<AuthProvider>
+								<Finance />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/social'
+						element={
+							<AuthProvider>
+								<Social />
+							</AuthProvider>
+						}></Route>
+
+					<Route
+						path='/hobbies'
+						element={
+							<AuthProvider>
+								<Hobbies />
+							</AuthProvider>
+						}></Route>
 				</Routes>
 			</BrowserRouter>
 		</div>
