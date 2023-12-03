57016f45ef086424f33dc5ee96803258340ee3b4
Tomas del Real <tdelrealb@gmail.com>
2023-12-03 16:04:56 -0300

    Create Funcional Components in Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { SignUp } from './pages/SignUp/SignUp';
+import {Dashboard} from './pages/Dashboard/Dashboard';
+import {Work} from './pages/Work/Work';
+import {Studies} from './pages/Studies/Studies';
+import {Trip} from './pages/Trip/Trip';
+import {Personal} from './pages/Personal/Personal';
+import {Finance} from './pages/Finance/Finance';
+import {Social} from './pages/Social/Social';
+import {Hobbies} from './pages/Hobbies/Hobbies';
+
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
---
+++ client/src/pages/Social/Social.jsx
@@ -0,0 +1,7 @@
+export const Social = () => {
+	return (
+		<div>
+			<h1>Social</h1>
+		</div>
+	);
+};
