c5a7104bf156509b1eeb8613b4e42e3eeff3c949
Tomas del Real <tdelrealb@gmail.com>
2023-12-04 19:02:55 -0300

    Add Login Page and funcionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home';
+import {Login} from './pages/Login/Login';
 
 export const App = () => {
 	return (
@@ -9,6 +10,7 @@ export const App = () => {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
