01e80ae84cf9432d9292456de11b13202077a3f8
Tomas del Real <tdelrealb@gmail.com>
2023-12-03 21:19:49 -0300

    Update AuthProvider

--- client/src/utils/AuthProvider.jsx
+++ client/src/utils/AuthProvider.jsx
@@ -1,7 +1,6 @@
 import { Navigate } from 'react-router-dom';
-import {PropTypes} from 'prop-types';
+import { PropTypes } from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
 	const token = sessionStorage.getItem('token');
 
