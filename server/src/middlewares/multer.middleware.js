a6a09df461b9c1bdae32ac812a9dd36dfd0ebd27
IWKLOHN <141165871+IWKLOHN@users.noreply.github.com>
2023-12-14 00:01:05 -0300

    Default image, user y note models, user y note controllers

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,10 @@
+import multer from 'multer';
+
+const upload = multer({ 
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 1024 * 1024 * 5 // MAX 5MB
+    } 
+});
+
+export default upload;
